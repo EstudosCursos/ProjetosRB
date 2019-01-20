a8272494d265105ba72a8aa55c5bc384684ff476
Rodrigo Benedetti <rodrigobenedetti@outlook.com.br>
2019-01-20 11:01:24 -0200

    Java parte 5

---
+++ curso-java-parte-5-6/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -0,0 +1,15 @@
+package br.com.bytebank.banco.teste;
+
+public class TesteString {
+		public static void main(String[] args) {
+		
+			String nome = "Alura"; //Object literals
+			//String outro = new String("Alura");
+			//String outra = outra.replace("A", "a");
+			
+			String outra = nome.toLowerCase();
+			System.out.println(nome);
+			System.out.println(outra);
+		}
+
+}
